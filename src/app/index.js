14e0e2a97030a5a20111a72546f61f2dcd722b3b
Shapovalenko Dmitrij <boozz@Shapovalenkos-iMac.local>
2015-05-22 14:20:06 -0700

    Change angular-ui-route to ngRoute

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,19 @@
 'use strict';
 
-angular.module('puzzle', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      })
-      .state('page', {
-        url: '/:node',
-        templateUrl: 'app/page/page.html',
-        controller: 'PageCtrl'
-      });
-    $urlRouterProvider.otherwise('/');
+angular.module('puzzle', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute', 'ui.bootstrap'])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider.
+          when('/', {
+            templateUrl: 'app/main/main.html',
+            controller: 'MainCtrl'
+          }).
+          when('/:node', {
+            templateUrl: 'app/page/page.html',
+            controller: 'PageCtrl'
+          }).
+          otherwise({
+            redirectTo: '/'
+          });
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
