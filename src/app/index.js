0e2670ccf8906f35eddfd0eb902b1f15f397aeb3
Shapovalenko Dmitrij <boozz@Shapovalenkos-iMac.local>
2015-05-24 10:55:18 +0300

    Change ngRoute to angular-ui-router

--- src/app/index.js
+++ src/app/index.js
@@ -1,23 +1,33 @@
 'use strict';
 
-angular.module('puzzle', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute', 'ui.bootstrap'])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider.
-          when('/', {
-            templateUrl: 'app/main/main.html',
-            controller: 'MainCtrl'
-          }).
-          when('/:node', {
-            templateUrl: 'app/page/page.html',
-            controller: 'PageCtrl'
-          }).
-          otherwise({
-            templateUrl: 'app/notfound/notfound.html',
-            controller: 'NotFoundCtrl'
-          });
+angular.module('puzzle', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('notfound', {
+        // url: '/notfound',
+        templateUrl: 'app/notfound/notfound.html',
+        controller: 'NotFoundCtrl'
+      })
+      .state('notfound.page', {
+        url: '/notfound',
+        templateUrl: 'app/notfound/notfound.html',
+        controller: 'NotFoundCtrl'
+      })
+      .state('page', {
+        url: '/:node',
+        templateUrl: "app/page/page.html",
+        controller: "PageCtrl"
+      });
+    $urlRouterProvider.when('', '/');
+    $urlRouterProvider.otherwise('/notfound');
     $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
+      enabled: true,
+      requireBase: false
     });
   })
 ;
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,9 +1,15 @@
 'use strict';
 
-function MainCtrl($scope) {
+function MainCtrl($scope, Post, $state) {
 
     var date = new Date();
     $scope.year = date.getFullYear();
+
+    Post.query().$promise.then(function(result) {
+        $scope.posts = result;
+    }, function(error) {
+        $state.go('notfound');
+    });
 
 }
 angular.module('puzzle')
